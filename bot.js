1e9cb99a436392ea4db280adb20f38824be1d621
Michael Malachowski <mikitymike@gmail.com>
2018-02-05 12:49:31 -0600

    move trtl address to config and clean up

--- bot.js
+++ bot.js
@@ -1,58 +1,41 @@
+// Load up delay library
 const delay = require('delay');
 
 // Load up the discord.js library
 const Discord = require("discord.js");
 
-// This is your client. Some people call it `bot`, some people call it `self`, 
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
+// This is the client
 const client = new Discord.Client();
 
 // Here we load the config.json file that contains our token and our prefix values. 
 const config = require("./config.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
-
-const TRTLAddress = "TRTLuwXHrsG2mFQ7hGvicXCNRRJWit3JVPS5ArN5YrBsim4CWRADtEBaAUaVDRNNCrjTYHku4UuUCCt7AfuR8EJzezRoQkfZ97q";
-//const TRTLAddress = "TRTLv24QwiYRWHcigukwuGaDrU6kKY7k13tDpAdAw5hCcdCAbZwmACTHRtzAQCfTYLRWYNrcVjpwkQ3LT4UCd81154t82nbVi1G";
+// config.address contains the TRTLAddress
 
 const maxAddressSendDelay = 20000; //Milliseconds
 const maxReactDelay = 5000; //Milliseconds
+const minDelay = 5000; //Milliseconds
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-    // docs refer to as the "ClientUser".
-    //    console.log(client.emojis.find("name", "ayy"));
-});
-
-client.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-});
-
-client.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 });
 
 
 client.on("message", async message => {
     // This event will run on every single message received, from any channel or DM.
     
-    // It's good practice to ignore other bots. This also makes your bot ignore itself
-    // and not get into a spam loop (we call that "botception").
+    //Ignore if author isn't a bot
     if(!message.author.bot) return;
 
-    //Got a message from bot named TurtleRainDance
+    //Got a message from bot named TurtleBotRain
     if((message.author.username == "TurtleBotRain")){
-	console.log("got msg from trtlbotrain");
-	console.log("msg content"+message.content);
     
 	if((message.channel.type == "dm")){
-	console.log("got dm from trtlbotrain");
-	    
+
+	    console.log("Got DM from TurtleBotRain: "+message.content);	    
+
 	    //Get the message contents
 	    let msgContent = message.content
 	    
@@ -63,16 +46,14 @@ client.on("message", async message => {
 
 	    // If this message has an emoji
 	    if(emojiString){
-		console.log("got dm: " + msgContent);
-		console.log("got emoji: " + emojiString);
-		
+
 		// Get the channel we want
 		let testGuild = client.guilds.find("name","TurtleCoin");
 		let testChannel = testGuild.channels.find("name", "raindance");
 
 		// Get the last message in that channel
 		testChannel.fetchMessage(testChannel.lastMessageID)
-		    .then(delay((Math.random()*maxReactDelay)+5000))
+		    .then(delay((Math.random()*maxReactDelay)+minDelay))
 		    .then(fetchedMsg => {
 
 			// If we got a message
@@ -80,7 +61,7 @@ client.on("message", async message => {
 
 			    // Send the reaction
 			    fetchedMsg.react(testGuild.emojis.find("name",emojiString));
-			    console.log("sent reac");
+			    console.log("Reacted to latest message in raindance");
 			}
 		    });
 	    }
@@ -89,32 +70,35 @@ client.on("message", async message => {
     }
 });
 
+// This event will run on every single message received, from any channel or DM.
 client.on("messageUpdate", (oldMessage, newMessage) => {
-    // This event will run on every single message received, from any channel or DM.
+
     var message = newMessage;
 
     //TODO change to bot
     //Got a message from bot named TurtleRainDance
-    console.log("message updated");
-    console.log("author"+message.author.username);
-    console.log("bot?:"+message.author.bot);
+    console.log("message from "+message.author.username+" updated");
 
-    if((message.author.username == "TurtleBotRain") && (message.author.bot)){
+    // If this is a message from TurtleBotRain and it contains an embed and is in the raindance channel
+    if((message.author.username == "TurtleBotRain") && (message.author.bot) && (message.embeds) && (message.channel.name == "raindance")){
 
 	console.log(message.embeds[0].title);
+	console.log(message.embeds[0].description);
+    
 	// Its a rain announcement
 	// TODO check message contents
 
 	var content = message.embeds[0].title;
-	console.log("channel"+message.channel.name);
-	if((message.channel.name == "raindance") && content.indexOf("QUICK") !== -1){
-	    // send him our trtl key
+
+	if(content.indexOf("QUICK") !== -1){
+	    
+	    // send him our trtl key after delay
 	    console.log("sending key after delay");
-	    //TODO wait a bit until bot is accepting keys
-	    delay((Math.random()*maxAddressSendDelay)+5000)
+	    
+	    delay((Math.random()*maxAddressSendDelay)+minDelay)
 		.then(() => {
-		    message.author.send(TRTLAddress);
-	    	    console.log("sent dm");
+		    message.author.send(config.address);
+	    	    console.log("Sent address to TurtleBotRain");
 		});
 	}	
 	return;
